6b71dc601f7805fb6aac53f5bfa5e10edeca3783
Clayton-Toste <claytontoste@gmail.com>
2021-11-12 16:10:18 -0800

    Started Webcam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
@@ -34,9 +34,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 @TeleOp(name = "DRIVE-TEST", group = "Pushbot")
 public class DriveTest extends LinearOpMode {
     private DcMotorEx motorFL, motorFR, motorBL, motorBR;
+    private OpenCvWebcam webcam;
+
 
     @Override
     public void runOpMode() {
@@ -45,10 +56,13 @@ public class DriveTest extends LinearOpMode {
         waitForStart();
 
         initMotors();
+        initWebcam();
 
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             telemetry.update();
         }
+
+        webcam.stopStreaming();
     }
 
     private void initMotors() {
@@ -81,5 +95,82 @@ public class DriveTest extends LinearOpMode {
         motorFR.setVelocity(200);
         motorBL.setVelocity(200);
         motorBR.setVelocity(200);
+    }
+
+    private void initWebcam() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(new Pipeline());
+    }
+
+    private void run() {
+    }
+
+    class Pipeline extends OpenCvPipeline {
+        boolean viewportPaused;
+
+        /*
+         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+         * highly recommended to declare them here as instance variables and re-use them for
+         * each invocation of processFrame(), rather than declaring them as new local variables
+         * each time through processFrame(). This removes the danger of causing a memory leak
+         * by forgetting to call mat.release(), and it also reduces memory pressure by not
+         * constantly allocating and freeing large chunks of memory.
+         */
+
+        @Override
+        public Mat processFrame(Mat input) {
+            /*
+             * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+             * will only dereference to the same image for the duration of this particular
+             * invocation of this method. That is, if for some reason you'd like to save a copy
+             * of this particular frame for later use, you will need to either clone it or copy
+             * it to another Mat.
+             */
+
+            /*
+             * Draw a simple box around the middle 1/2 of the entire frame
+             */
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols() / 4,
+                            input.rows() / 4),
+                    new Point(
+                            input.cols() * (3f / 4f),
+                            input.rows() * (3f / 4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            /**
+             * NOTE: to see how to get data from your pipeline to your OpMode as well as how
+             * to change which stage of the pipeline is rendered to the viewport when it is
+             * tapped, please see {@link PipelineStageSwitchingExample}
+             */
+
+            return input;
+        }
+
+        @Override
+        public void onViewportTapped() {
+            /*
+             * The viewport (if one was specified in the constructor) can also be dynamically "paused"
+             * and "resumed". The primary use case of this is to reduce CPU, memory, and power load
+             * when you need your vision pipeline running, but do not require a live preview on the
+             * robot controller screen. For instance, this could be useful if you wish to see the live
+             * camera preview as you are initializing your robot, but you no longer require the live
+             * preview after you have finished your initialization process; pausing the viewport does
+             * not stop running your pipeline.
+             *
+             * Here we demonstrate dynamically pausing/resuming the viewport when the user taps it
+             */
+
+            viewportPaused = !viewportPaused;
+
+            if (viewportPaused) {
+                webcam.pauseViewport();
+            } else {
+                webcam.resumeViewport();
+            }
+        }
     }
 }
